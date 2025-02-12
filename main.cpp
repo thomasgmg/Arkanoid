19bfd9ad88486c82c59b0c0b9eac1c08a7b7cb56
thomasgmg <cabralzinho.mg@gmail.com>
2025-02-12 10:02:42 +0100

    implement bouncing ball
    -resizable window
    -bouncing ball

--- main.cpp
+++ main.cpp
@@ -1,41 +1,62 @@
 #include "raylib.h"
 
 int main(void) {
-  // Initialization
-  //--------------------------------------------------------------------------------------
-  const int screenWidth = 800;
-  const int screenHeight = 450;
+    //--------------------------------------------------------------------------------------
+    const int screenWidth = 800;
+    const int screenHeight = 450;
 
-  InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
+    SetConfigFlags(FLAG_MSAA_4X_HINT | FLAG_WINDOW_RESIZABLE);
+    InitWindow(screenWidth, screenHeight, "raylib [shapes] example - basic ball");
 
-  SetTargetFPS(60); // Set our game to run at 60 frames-per-second
-  //--------------------------------------------------------------------------------------
+    Vector2 ballPosition = {GetScreenWidth()/2.0f, GetScreenHeight()/2.0f};
+    Vector2 ballSpeed = {5.0f, 4.0f};
+    int ballRadius = 20;
 
-  // Main game loop
-  while (!WindowShouldClose()) // Detect window close button or ESC key
-  {
-    // Update
-    //----------------------------------------------------------------------------------
-    // TODO: Update your variables here
-    //----------------------------------------------------------------------------------
+    bool pause = 0;
+    int framesCounter = 0;
 
-    // Draw
-    //----------------------------------------------------------------------------------
-    BeginDrawing();
+    SetTargetFPS(60);
+    //--------------------------------------------------------------------------------------
 
-    ClearBackground(RAYWHITE);
+    // Main game loop
+    while (!WindowShouldClose()) // Detect window close button or ESC key
+    {
+        // Update
+        //----------------------------------------------------------------------------------
+        // TODO: Update your variables here
+        //----------------------------------------------------------------------------------
 
-    DrawText("Congrats! You created your first window!", 190, 200, 20,
-             LIGHTGRAY);
+        if (IsKeyPressed(KEY_SPACE)) pause = !pause;
+        if (!pause){
+            ballPosition.x += ballSpeed.x;
+            ballPosition.y += ballSpeed.y;
 
-    EndDrawing();
-    //----------------------------------------------------------------------------------
-  }
+            if ((ballPosition.x >= (GetScreenWidth() - ballRadius)) || (ballPosition.x <= ballRadius)) ballSpeed.x *= -1.0f;
+            if ((ballPosition.y >= (GetScreenHeight() - ballRadius)) || (ballPosition.y <= ballRadius)) ballSpeed.y *= -1.0f;
+        }
+        else framesCounter++;
 
-  // De-Initialization
-  //--------------------------------------------------------------------------------------
-  CloseWindow(); // Close window and OpenGL context
-  //--------------------------------------------------------------------------------------
+        // Draw
+        //----------------------------------------------------------------------------------
+        BeginDrawing();
 
-  return 0;
+        ClearBackground(DARKBLUE);
+
+        DrawCircleV(ballPosition, (float)ballRadius, MAROON);
+        DrawText("Press space to pause ball movement", 10, GetScreenHeight() - 25, 20, BLACK);
+
+        if (pause && ((framesCounter/30%2))) DrawText("Paused", 350, 200, 20, BLACK);
+
+        DrawFPS(10, 10);
+
+        EndDrawing();
+        //----------------------------------------------------------------------------------
+    }
+
+    // De-Initialization
+    //--------------------------------------------------------------------------------------
+    CloseWindow();
+    //--------------------------------------------------------------------------------------
+
+    return 0;
 }
